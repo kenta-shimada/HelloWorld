57041102844bf9de3918c26fc866bdfa3369b5cb
kenta.shimada <tinswitl@gmail.com>
2020-11-18 05:51:24 +0900

    Initial Commit

---
+++ holloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  holloworld
+//
+//  Created by 島田剣太 on 2020/11/18.
+//  Copyright © 2020 kenta.shimada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
